75cbcad59e78b60d0a91df824f6d0002a3f98eee
chenyee1976 <chenwallpaper@gmail.com>
2026-07-30 15:45:42 +0800

    fix: disable PWA Service Worker caching in flutter_bootstrap.js to force live fetching

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,7 +38,5 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 
 _flutter.loader.load({
-  serviceWorkerSettings: {
-    serviceWorkerVersion: "3104267265"
-  }
+  serviceWorkerSettings: null
 });
